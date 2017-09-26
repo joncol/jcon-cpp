fcdb98372e4c1b64a575d32f8d567ad0f62b7449
Pavel Melnikov <positron96@gmail.com>
2017-09-26 18:14:24 +0700

    Update json_rpc_debug_logger.h
    Added logDebug (missing?)

--- src/jcon/json_rpc_debug_logger.h
+++ src/jcon/json_rpc_debug_logger.h
@@ -8,6 +8,7 @@ namespace jcon {
 class JCON_API JsonRpcDebugLogger : public JsonRpcLogger
 {
 public:
+    void logDebug(const QString& message) override;
     void logInfo(const QString& message) override;
     void logWarning(const QString& message) override;
     void logError(const QString& message) override;
