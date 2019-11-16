aa2cae59e466dea8cb6e0c3b628cabe9221bbd34
Jonas Collberg <jonas.collberg@gmail.com>
2019-11-16 12:18:42 +0100

    Add dummy unit test
    Using boost's unit test framework.

---
+++ src/test/test_jcon_cpp/test_jcon_cpp.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+BOOST_AUTO_TEST_SUITE ( jcon_cpp_test_suite )
+
+BOOST_AUTO_TEST_CASE( dummy_test )
+{
+    BOOST_CHECK_EQUAL(1, 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ src/test/test_jcon_cpp/test_module.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE TestJconCpp
+
+#include <boost/test/unit_test.hpp>
