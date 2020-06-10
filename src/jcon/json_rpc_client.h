5c202e7fc7f47472e5f1eb3fd9f984ac9704a8d9
OpenSourceDev <opensourcedev@protonmail.com>
2020-06-10 16:49:33 +0200

    Update src/jcon/json_rpc_client.h
    Naming conventions cleaning
    
    Co-authored-by: Jonas Collberg <jonas.collberg@gmail.com>

--- src/jcon/json_rpc_client.h
+++ src/jcon/json_rpc_client.h
@@ -194,9 +194,8 @@ JsonRpcClient::doCall(const QString& method, bool async, Ts&&... args)
     std::tie(request, req_json_obj) = prepareCall(method);
 
     QVariantList param_list;
-    constexpr std::size_t numb_args = sizeof...(Ts);
-    if ( numb_args != 0 )
-    {
+    constexpr std::size_t arg_count = sizeof...(Ts);
+    if (arg_count != 0) {
         convertToQVariantList(param_list, std::forward<Ts>(args)...);
         req_json_obj["params"] = QJsonArray::fromVariantList(param_list);
     }
